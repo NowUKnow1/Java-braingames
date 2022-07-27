5df256bbb3b85a5b1bedae8844b254628772fcac
Vadim Tentyuk <vadim.tentyuk97@gmail.com>
2022-07-27 20:18:48 +1000

    Get files to App

--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -5,7 +5,7 @@ import  java.util.Scanner;
 public class Cli {
     public static String getName() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("May I Have your name?");
+        System.out.print("May I Have your name? ");
         String userName = sc.next();
         return userName;
     }
