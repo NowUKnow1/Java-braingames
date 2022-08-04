6830cc9a3a2327aa4c0a1d2fe4df55566777d3f8
NowUKnow1 <108438969+NowUKnow1@users.noreply.github.com>
2022-08-04 21:30:50 +1000

    Update PrimeNumbers.java

--- src/main/java/hexlet/code/games/PrimeNumbers.java
+++ src/main/java/hexlet/code/games/PrimeNumbers.java
@@ -3,16 +3,12 @@ package hexlet.code.games;
 import java.util.Random;
 
 public final class PrimeNumbers implements Game {
+    
     private static final int MAX_NUMBER = 101;
-
     private static final String PRIME = "yes";
-
     private static final String NOT_PRIME = "no";
-
     private static final String GAME_NAME = "Prime Numbers";
-
     private static final String GAME_GOAL = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
-
 
     public String getName() {
         return GAME_NAME;
@@ -23,10 +19,9 @@ public final class PrimeNumbers implements Game {
     }
 
     public String[] getAnswerAndQuestion() {
+        
         boolean checkForPrime = true;
-
         Random random = new Random();
-
         int randomNumber = random.nextInt(MAX_NUMBER);
 
         for (int i = 2; i < randomNumber; i++) {
@@ -39,7 +34,6 @@ public final class PrimeNumbers implements Game {
 
         String[] answerAndQuestion = new String[2];
         answerAndQuestion[0] = (checkForPrime) ? PRIME : NOT_PRIME;
-
         answerAndQuestion[1] = Integer.toString(randomNumber);
 
         return answerAndQuestion;
