990838a37e4eca608c2944d3abaafe4be67b8c94
NowUKnow1 <108438969+NowUKnow1@users.noreply.github.com>
2022-08-04 21:28:52 +1000

    Update EvenNumbers.java

--- src/main/java/hexlet/code/games/EvenNumbers.java
+++ src/main/java/hexlet/code/games/EvenNumbers.java
@@ -1,16 +1,14 @@
 package hexlet.code.games;
+
 import java.util.Random;
+
 public final class EvenNumbers implements Game {
+    
     private static final String YES = "yes";
-
     private static final String NO = "no";
-
     private static final int MAX_NUMBER = 100;
-
     private static final String GAME_NAME = "Even Numbers";
-
     private static final String GAME_GOAL = "Answer 'yes' if number yes otherwise answer 'no'.";
-
 
     public String getName() {
         return GAME_NAME;
@@ -21,12 +19,10 @@ public final class EvenNumbers implements Game {
     }
 
     public String[] getAnswerAndQuestion() {
+        
         Random random = new Random();
-
         int randomNumber = random.nextInt(MAX_NUMBER) + 1;
-
         String[] answerAndQuestion = new String[2];
-
         answerAndQuestion[1] = Integer.toString(randomNumber);
 
         if (randomNumber % 2 == 0) {
