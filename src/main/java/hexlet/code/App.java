6bea1f66c89a10d73421381254cdf84f66e59bca
NowUKnow1 <108438969+NowUKnow1@users.noreply.github.com>
2022-08-04 20:59:13 +1000

    Update App.java

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -35,7 +35,6 @@ public class App {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
         System.out.println("Please enter the game number and press Enter");
         System.out.println("1 - Greet");
         for (Map.Entry<Integer, Game> game : GAMES.entrySet()) {
@@ -44,7 +43,6 @@ public class App {
         System.out.println("0 - Exit");
         System.out.print("Your choice: ");
         int gameNumber = sc.nextInt();
-        
         switch (gameNumber) {
             case 0 -> System.out.println("The champ is here");
             case 1 -> {
