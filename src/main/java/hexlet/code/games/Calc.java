7ed0718f93a2db811a66659579f3b443dbe0c182
NowUKnow1 <108438969+NowUKnow1@users.noreply.github.com>
2022-08-04 21:27:41 +1000

    Update Calc.java

--- src/main/java/hexlet/code/games/Calc.java
+++ src/main/java/hexlet/code/games/Calc.java
@@ -6,21 +6,14 @@ import java.util.Random;
 public final class Calc implements Game {
 
     private static final int MAX_FIRST_NUMBER = 101;
-
     private static final int MAX_SECOND_NUMBER = 101;
-
     private static final int NEEDED_NUMBER_OF_OPERATIONS = 3;
-
     private static final int ADDITION = 1;
-
     private static final int SUBTRACTION = 2;
-
     private static final int MULTIPLY = 3;
-
+    
     private static final String GAME_NAME = "Calc";
-
     private static final String GAME_GOAL = "What is the result of the expression?";
-
 
     public String getName() {
         return GAME_NAME;
@@ -33,9 +26,7 @@ public final class Calc implements Game {
     public String[] getAnswerAndQuestion() {
         Random random = new Random();
         int firstNumber = random.nextInt(MAX_FIRST_NUMBER);
-
         int secondNumber = random.nextInt(MAX_SECOND_NUMBER);
-
         int chooseMath = random.nextInt(NEEDED_NUMBER_OF_OPERATIONS) + 1;
 
         String[] answerAndQuestion = new String[2];
